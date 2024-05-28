e5a85bb0d78adbac4a90f8a4c6d5dcbffc0c7c37
Duarte Leitão <duarte0903@gmail.com>
2024-05-28 17:36:25 +0100

    routes API

--- API/controllers/uc.js
+++ API/controllers/uc.js
@@ -20,14 +20,14 @@ module.exports.insert = uc => {
     return Uc.create(uc)
 };
 
-// remover uma uc por id
-module.exports.remove = id => {
-    return Uc.deleteOne({_id: id})
+// remover uma uc por id (so o coordenador da uc pode remover)
+module.exports.remove = (id, idDocente) => {
+    return Uc.deleteOne({_id: id, 'docentes.0': idDocente})
 };
 
-// atualizar uma uc por id
-module.exports.update = (id, uc) => {
-    return Uc.updateOne.findByIdAndUpdate(id, uc)
+// atualizar uma uc por id (so o coordenador da uc pode atualizar)
+module.exports.update = (id, uc, idDocente) => {
+    return Uc.updateOne({_id: id, 'docentes.0': idDocente}, uc)
 }
 
 // listar alunos inscritos numa uc
--- API/routes/index.js
+++ API/routes/index.js
@@ -1,24 +1,121 @@
 var express = require('express');
 var router = express.Router();
-var UC = require('../controllers/uc');
-var User = require('../controllers/user');
 var jwt = require('jsonwebtoken')
 var multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 var fs = require('fs')
+var jsonfile = require('jsonfile')
 
-// GET /cadeiras (do user)
+var Uc = require('../controllers/uc');
+var User = require('../controllers/user');
+var Ficheiro = require('../controllers/ficheiro');
 
-// GET /cadeiras/:_id
+function auth(req, res, next) {
+    let token = req.body.token || req.query.token
+    delete req.body.token
+    delete req.query.token
+    if (token) {
+        jwt.verify(token, "EngWeb2023", function (e, payload) {
+            if (e) {
+                res.status(401).jsonp({error: e})
+            } 
+            
+            else {
+                req.user = payload
+                next()
+            }
+        })
+    } else {
+        res.status(401).jsonp({error: 'Error: No token was provided'})
+    }
+}
+
+// GET /cadeiras
+router.get('/cadeiras', function(req, res) {
+    if (req.user) {
+        Uc.list()
+            .then(data => res.jsonp(data))
+            .catch(error => res.status(500).jsonp(error))
+    }
+});
 
 // GET /cadeiras/:_id/alunos
+router.get('/cadeiras/:_id/alunos', auth, function(req, res) {
+    if (req.user) {
+        Uc.lookUp(req.params._id)
+            .then(data => res.jsonp(data.inscritos))
+            .catch(error => res.status(500).jsonp(error))
+    }
+});
 
 // GET /cadeiras/:_id/ficheiros
+router.get('/cadeiras/:_id/ficheiros', auth, function(req, res) {
+    if (req.user) {
+        Ficheiro.listCadeira(req.params._id)
+            .then(data => res.jsonp(data))
+            .catch(error => res.status(500).jsonp(error))
+    }
+});
 
-// GET /cadeiras/:_id/ficheiros/:_id
+// GET /cadeiras/:_id/ficheiros/:_idFicheiro
+router.get('/cadeiras/:_id/ficheiros/:_idFicheiro', auth, function(req, res) {
+    if (req.user) {
+        Ficheiro.lookUp(req.params._idFicheiro)
+            .then(data => res.jsonp(data))
+            .catch(error => res.status(500).jsonp(error))
+    }
+});
 
-// POST /cadeiras/:_id/ficheiros
+// GET /cadeiras/:_id/ficheiros/:_idFicheiro/download
+router.get('/cadeiras/:_id/ficheiros/:_idFicheiro/download', auth, function(req, res) {
+    if (req.user) {
+        Ficheiro.lookUp(req.params._idFicheiro)
+            .then(data => {
+                res.download(data.path)
+            })
+            .catch(error => res.status(500).jsonp(error))
+    }
+});
 
-// DELETE /cadeiras/:_id/ficheiros/:_id
+// GET users/:_id/cadeiras
+router.get('/users/:_id/cadeiras', auth, function(req, res) {
+    if (req.user._id === req.params._id) {
+        User.getCadeiras(req.params._id)
+            .then(data => res.jsonp(data.cadeiras))
+            .catch(error => res.status(500).jsonp(error))
+    }
+});
+
+// GET users/:_id/cadeiras/:_idCadeira
+router.get('/users/:_id/cadeiras/:_idCadeira', auth, function(req, res) {
+    if (req.user._id === req.params._id) {
+        var uc = Uc.lookUp(req.params._idCadeira)
+
+        for (var i = 0; i < uc.inscritos.length; i++) {
+            if (uc.inscritos[i] == req.params._id) {
+                res.jsonp(uc)
+                return
+            }
+        }
+
+        res.status(401).jsonp({error: 'User not enrolled in this course'})
+    }
+});
+
+// POST / (docentes ou admin)
+router.post('/', auth, function(req, res) {
+    // testar se o level do user é docente ou admin
+    if (req.user.nivel != 'docente' && req.user.nivel != 'admin') {
+        res.status(401).jsonp({error: 'User not authorized'})
+        return
+    }
+    Uc.insert(req.body)
+        .then(data => res.jsonp(data))
+        .catch(error => res.status(500).jsonp(error))
+});
+
+// POST /cadeiras/:_id/ficheiros (docentes ou admin)
+
+// DELETE /cadeiras/:_id/ficheiros/:_idFicheiro (docentes ou admin)
 
 module.exports = router;
